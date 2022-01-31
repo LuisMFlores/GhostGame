cf9030a5a17d634840fbabf786a9a1a1cf810ccc
Luis M. Flores <flores_luis_m@outlook.com>
2022-01-31 15:23:35 -0500

    ghost game initializer

--- Ghost.rb
+++ Ghost.rb
@@ -1,3 +1,20 @@
+require "byebug"
+require_relative "Player"
+
 class Ghost
 
+    def initialize(*players)
+        @dictionary = Set.new(File.readlines("dictionary.txt").map(&:chomp))
+        @players = players
+        @fragment = ""
+        @losses = Hash.new { |loss, player| loss[player] = 0 }
+    end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+    ghost_game = Ghost.new(
+        Player.new("Luis"),
+        Player.new("Merlin")
+    )  
 end
