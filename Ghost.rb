3cc3a4e520f70684a645a966f420d7f4547155e9
Luis M. Flores <flores_luis_m@outlook.com>
2022-01-31 15:34:51 -0500

    winner, remaining_players, game_over? methods implemented

--- Ghost.rb
+++ Ghost.rb
@@ -3,11 +3,35 @@ require_relative "Player"
 
 class Ghost
 
+    MAX_LOSS_COUNT = 5
+
     def initialize(*players)
         @dictionary = Set.new(File.readlines("dictionary.txt").map(&:chomp))
         @players = players
         @fragment = ""
-        @losses = Hash.new { |loss, player| loss[player] = 0 }
+        @losses = Hash.new { |loss, player| losses[player] = 0 }
+    end
+
+    def run
+        play_round until game_over?
+        puts "#{Winner} is the winner!"
+    end
+
+    def play_round
+
+    end
+
+    def game_over?
+        remaining_players == 1
+    end
+
+    def remaining_players
+        losses.count { |_, v| v < MAX_LOSS_COUNT }
+    end
+
+    def winner
+        (player, _) = losses.find { |_, losses| losses < MAX_LOSS_COUNT }
+        player
     end
 
 end
