802dca2c55dc5676e79d39e9374e65bd2f7f5626
Luis M. Flores <flores_luis_m@outlook.com>
2022-02-03 13:06:14 -0500

    initialize added

--- Ghost.rb
+++ Ghost.rb
@@ -1,3 +1,19 @@
-require "set"
-require_relative "Player"
+require "Set"
 require "byebug"
+require_relative "Player"
+
+class GhostGame
+
+    def initialize(*players)
+        @players = players
+        @dictionary = Set.new(File.readlines("dictionary.txt").map(&:chomp))
+        @fragment = ""
+        @losses = Hash.new { |losses, player| losses[player] = 0 }
+    end
+
+end
+
+game = GhostGame.new(
+    Player.new("Luis"),
+    Player.new("Merlin")
+)
