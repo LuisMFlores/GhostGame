2181a8b8c9ab3f4a9557249366f945a8b440f7c6
Luis M. Flores <flores_luis_m@outlook.com>
2022-02-03 13:12:19 -0500

    run method added

--- Ghost.rb
+++ Ghost.rb
@@ -4,11 +4,34 @@ require_relative "Player"
 
 class GhostGame
 
+    
+
     def initialize(*players)
         @players = players
         @dictionary = Set.new(File.readlines("dictionary.txt").map(&:chomp))
         @fragment = ""
         @losses = Hash.new { |losses, player| losses[player] = 0 }
+    end
+
+    def run
+        play_round until game_over?
+        puts "#{winner} is the winner!"
+    end
+
+    private
+
+    attr_reader :players, :dictionary, :fragment, :losses
+
+    def winner
+
+    end
+
+    def play_round
+
+    end
+
+    def game_over?
+
     end
 
 end
